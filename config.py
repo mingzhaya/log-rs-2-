8500097b81c238d3fc65d863cabb43b4cbb9a878
Mingzhe Lin <mingzhaya@gmail.com>
2020-08-04 19:22:57 +0800

    Main file and config

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+from pathlib import Path
+basedir = Path(__file__).parent.absolute()
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'srsr_derauqs'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                            'sqlite:///' + str(basedir / 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = []
