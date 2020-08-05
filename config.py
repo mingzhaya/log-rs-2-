b79a49accca43dd6c182f5b313538df88fe976ca
Mingzhe Lin <mingzhaya@gmail.com>
2020-08-05 23:58:57 +0800

    Add config for md display per page

--- config.py
+++ config.py
@@ -8,6 +8,8 @@ class Config(object):
                             'sqlite:///' + str(basedir / 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    MAPDROPS_PER_PAGE = 10
+
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
