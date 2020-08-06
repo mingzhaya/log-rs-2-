e2e8c0fe775e4b2f7f2dc2d2e51aa3a5594dc7bb
Mingzhe Lin <mingzhaya@gmail.com>
2020-08-06 23:25:34 +0800

    Add new tables to shell

--- rsrs.py
+++ rsrs.py
@@ -1,6 +1,6 @@
 from app import app, db
-from app.models import User, MapDrop
+from app.models import User, MapDrop, ItemDrop, Quest
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'MapDrop': MapDrop}
+    return {'db': db, 'User': User, 'MapDrop': MapDrop, 'ItemDrop': ItemDrop, 'Quest': Quest}
